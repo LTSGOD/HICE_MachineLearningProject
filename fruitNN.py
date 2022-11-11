6ab5e1fed929292a88c6b1e2f3d7b8e79e420132
hyeyoon <hyoon1129@naver.com>
2022-11-12 01:25:28 +0900

    add

--- fruitNN.py
+++ fruitNN.py
@@ -53,15 +53,10 @@ class SoftmaxWithLoss:
         self.loss = cross_entropy_error(self.y, self.t)
 
         return self.loss
+    
     def backward(self, dout=1):
         batch_size = self.t.shape[0]
         dx = (self.y-self.t) / batch_size
-        return dx
-
-    def backward(self, dout=1):
-        batch_size = self.t.shape[0]
-        dx = (self.y - self.t)/batch_size
-
         return dx
 
 
